bb1da31d8e603d77f7b919c9ce9687d12227ac9c
Arthur Vasconcelos <arthurgvasconcelos23@gmail.com>
2023-01-06 15:01:40 -0300

    teste

---
+++ src/main/java/br/com/coffeeandit/testdockerimage/TestDockerImageApplication.java
@@ -0,0 +1,13 @@
+package br.com.coffeeandit.testdockerimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDockerImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDockerImageApplication.class, args);
+	}
+
+}
